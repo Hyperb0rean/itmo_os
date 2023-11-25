09f188a9aacb0013a9dc03416adee624f8c112d9
hyperb0rean <jewellin02@gmail.com>
2023-11-25 21:19:00 +0300

    added C++ to kernel ad user spaces

--- xv6-riscv/user/hello.c
+++ xv6-riscv/user/hello.c
@@ -2,6 +2,6 @@
 #include "user/user.h"
 
 int main(int argc, char **argv) {
-    fprintf(1, "Hello world\n");
+    fprintf(1, "Hello world! It's just C program\n");
     exit(0);
 }
---
+++ xv6-riscv/user/hellocxx.cpp
@@ -0,0 +1,7 @@
+#include "cstdio"
+
+
+int main() {
+    std::printf("Hello world! It's C++ program\n");
+    return 0;
+}
